18af6b24da0aecf2b5f1b052fc975a3011c120c8
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-08-18 03:28:43 +0000

    [Start]4-14

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,14 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
-  
+  belongs_to :user
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.oepn(file_path),filename:'deafult-image.jpg',content_type: 'image/jpeg')
+    end
+      image
+  end
+
 end
